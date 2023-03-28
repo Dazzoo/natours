c30eadb8a194718118ca1ce2ee24208d3b04427a
Юра Шушанський <yuraks46@gmail.com>
2023-03-28 22:49:49 +0300

    get tour/id

--- natours/index.js
+++ natours/index.js
@@ -29,6 +29,30 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    let id = Number(req.params.id)
+
+    let tour = tours.find((t) => t.id === id)
+
+    if (!tour) {
+        if (id > tours.length) {
+            res.status(404).json({
+                status: 'fail',
+                message: 'Invalid tour ID',
+            })
+        }
+    } else {
+        res.status(200).json({
+            status: 'success',
+            body: {
+                tour,
+            },
+        })
+    }
+
+    res.send('End')
+})
+
 app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1
     const newObj = Object.assign({ id: newId }, req.body)
