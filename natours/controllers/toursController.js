955345c722d4c7fb62edf77db39e724b17e3c121
Yurii <yuraks46@gmail.com>
2023-04-03 16:01:54 +0300

    refactor

--- natours/controllers/toursController.js
+++ natours/controllers/toursController.js
@@ -31,7 +31,7 @@ module.exports.checkId = (req, res, next, value) => {
 module.exports.requiredParams = (req, res, next) => {
     if (!req.body.name || !req.body.price) {
         return res.status(400).json({
-            status: "bad request"
+            status: "Required params: name, price"
         })
     }
     console.log(!req.body.price)
