ec24481ea0a3dc47f0643e9ea6cb03ddd2c17fba
Yurii <yuraks46@gmail.com>
2023-05-01 22:56:31 +0300

    creating tour from TourSchema

--- natours/server.js
+++ natours/server.js
@@ -7,7 +7,7 @@ const app = require('./index')
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD)
 
 mongoose.connect(DB).then((db) => {
-    // console.log(db.connections)
+    console.log(db.connections)
     console.log('___________Connected!____________')
 })
 
@@ -29,7 +29,21 @@ const tourSchema = new mongoose.Schema({
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
-console.log(Tour)
+
+const TestTour = new Tour({
+    name: 'The Park Camper',
+    price: 497,
+})
+
+TestTour.save()
+    .then((doc) => {
+        console.log(doc)
+        console.log('__________Document has been saved succesfully__________')
+    })
+    .catch((err) => {
+        console.log(err)
+        console.log('__________Error__________')
+    })
 
 const port = process.env.PORT
 
