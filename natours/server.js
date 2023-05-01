e765ea477d316aa7a7c5d10d13beece5644b7b5a
Yurii <yuraks46@gmail.com>
2023-05-01 15:46:28 +0300

    tour schema

--- natours/server.js
+++ natours/server.js
@@ -7,9 +7,29 @@ const app = require('./index')
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD)
 
 mongoose.connect(DB).then((db) => {
-    console.log(db.connections)
+    // console.log(db.connections)
     console.log('___________Connected!____________')
 })
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        required: false,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
+console.log(Tour)
 
 const port = process.env.PORT
 
