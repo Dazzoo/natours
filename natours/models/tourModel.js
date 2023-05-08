3dcb9be4fe411071f60dcdc50dcae8ddfa312b27
Yurii <yuraks46@gmail.com>
2023-05-08 23:06:51 +0300

    add fields to the tourModel Tour Schema

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -6,14 +6,57 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
     },
-    rating: {
+    ratingsAverage: {
         type: Number,
         required: false,
         default: 4.5,
     },
+    ratingsQuantity: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    images: {
+        type: [String],
+        required: false,
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have an cover image'],
+        default: `${__dirname}/dev-data/img/new-tour-1.jpg`,
+    },
     price: {
         type: Number,
         required: [true, 'A tour must have a price'],
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration'],
+        default: 7,
+    },
+    maxGroupSize: {
+        type: Number,
+        required: false,
+    },
+    difficulty: {
+        type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: [true, 'A tour must have a description'],
+    },
+    summary: {
+        type: String,
+        required: false,
+    },
+    startDates: {
+        type: [Date],
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
     },
 })
 
