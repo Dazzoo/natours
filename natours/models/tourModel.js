7d80508888a99a7339eed0c18352708b7e078caa
Yurii <yuraks46@gmail.com>
2023-05-31 22:40:17 +0300

    vireual property in mongodb

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,63 +1,74 @@
 const mongoose = require('mongoose')
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+        },
+        ratingsAverage: {
+            type: Number,
+            required: false,
+            default: 4.5,
+        },
+        ratingsQuantity: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        images: {
+            type: [String],
+            required: false,
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have an cover image'],
+            default: `${__dirname}/dev-data/img/new-tour-1.jpg`,
+        },
+        price: {
+            type: Number,
+            required: false,
+        },
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration'],
+            default: 7,
+        },
+        maxGroupSize: {
+            type: Number,
+            required: false,
+        },
+        difficulty: {
+            type: String,
+            required: false,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        summary: {
+            type: String,
+            required: false,
+        },
+        startDates: {
+            type: [Date],
+            required: false,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
     },
-    ratingsAverage: {
-        type: Number,
-        required: false,
-        default: 4.5,
-    },
-    ratingsQuantity: {
-        type: Number,
-        required: false,
-        default: 0,
-    },
-    images: {
-        type: [String],
-        required: false,
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have an cover image'],
-        default: `${__dirname}/dev-data/img/new-tour-1.jpg`,
-    },
-    price: {
-        type: Number,
-        required: false,
-    },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration'],
-        default: 7,
-    },
-    maxGroupSize: {
-        type: Number,
-        required: false,
-    },
-    difficulty: {
-        type: String,
-        required: false,
-    },
-    description: {
-        type: String,
-        required: false,
-    },
-    summary: {
-        type: String,
-        required: false,
-    },
-    startDates: {
-        type: [Date],
-        required: false,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+)
+
+tourSchema.virtual('durationWeeks').get(function () {
+    console.log(this.duration)
+    return this.duration / 7
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
