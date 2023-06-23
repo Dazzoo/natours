26cb8f0a1ba304a391ad1622f9a3b92ea4a166ed
Yurii <yuraks46@gmail.com>
2023-06-23 20:26:51 +0300

    request error handling

--- natours/index.js
+++ natours/index.js
@@ -26,4 +26,23 @@ app.use('/api/v1/tours', tourRouter)
 
 app.use('/api/v1/users', usersRouter)
 
+app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on the server`)
+
+    err.status = 'fail'
+    err.statusCode = 404
+
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    })
+})
+
 module.exports = app
