518af7b415541fe73eda71e9003a2c40ecc8b98c
Yurii <yuraks46@gmail.com>
2023-06-25 20:49:10 +0300

    handle errors message for production

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,3 +1,21 @@
+const AppError = require('../utility/appError')
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateErrorDB = (err) => {
+    const message = `Duplicate field value: ${err.keyValue.name}. Please use another one!`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = (err) => {
+    const errorsList = Object.values(err.errors).map((el) => el.message)
+    const message = `Validation error: ${errorsList.join('. ')}`
+    return new AppError(message, 400)
+}
+
 const sendErrorProd = (err, res) => {
     if (err.isOperational) {
         res.status(err.statusCode).json({
@@ -18,6 +36,7 @@ const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
+        isOperational: err.isOperational,
         error: err,
         stack: err.stack,
     })
@@ -30,6 +49,11 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENVIROMENT === 'development') {
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENVIROMENT === 'production') {
-        sendErrorProd(err, res)
+        let error = { ...err }
+        if (err.name === 'CastError') error = handleCastErrorDB(error)
+        if (err.code === 11000) error = handleDuplicateErrorDB(error)
+        if (err.name === 'ValidationError')
+            error = handleValidationErrorDB(error)
+        sendErrorProd(error, res)
     }
 }
