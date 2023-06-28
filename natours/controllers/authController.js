914ddbe022eaefba858997ffda00d0173921a73d
Yurii <yuraks46@gmail.com>
2023-06-28 19:36:17 +0300

    sign jwt token

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,12 +1,18 @@
 const catchAsync = require('../utility/catchAsync')
 const User = require('../models/userModel')
 const AppError = require('../utility/appError')
+const jwt = require('jsonwebtoken')
 
 module.exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body)
 
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES,
+    })
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser,
         },
