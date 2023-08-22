85a376ec1ca5388fd25706f259e125eceffa3a96
Yurii <yuraks46@gmail.com>
2023-08-22 23:07:11 +0300

    fix

--- natours/index.js
+++ natours/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENVIROMENT === 'development') {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 250, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 250, // Limit each IP to [count] requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
