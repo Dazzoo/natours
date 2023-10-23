a2825eb48a2a32e0dc3decd6470f105f2bee3bd5
Yurii <yuraks46@gmail.com>
2023-10-23 23:37:21 +0300

    bookings model

---
+++ natours/models/bookingModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const bookingSchema = new mongoose.Schema(
+    {
+        price: {
+            type: Number,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        paid: {
+            type: Boolean,
+            default: true,
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: true,
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+)
+
+bookingSchema.pre(/^find/, function (next) {
+    this.populate([
+        {
+            path: 'tour',
+            select: 'name',
+        },
+    ]).populate('user')
+    next()
+})
+
+const Booking = mongoose.model('Booking', bookingSchema)
+
+module.exports = Booking
