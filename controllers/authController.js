a8f97a5aaed309cbcd1f5e8e8910f05b938a09f6
Yurii <yuraks46@gmail.com>
2023-10-28 00:28:06 +0300

    redis variables

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,8 +11,8 @@ const { serialize, parse } = require('cookie')
 
 const redisClient = createClient({
     socket: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
         tls: process.env.NODE_ENVIROMENT === 'production',
     },
 })
