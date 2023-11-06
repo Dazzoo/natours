b18d82a493a0a48d7730582c04f7d8df22973191
Dazzoo <yuraks46@gmail.com>
2023-11-06 23:27:13 +0200

    user/me debugging

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -30,13 +30,16 @@ module.exports.getAll = (Model) =>
 module.exports.getOne = (Model, populateOptions) =>
     catchAsync(async (req, res, next) => {
         const id = req.params.id
+        console.log('1')
 
         let doc = await Model.findById(id)
+        console.log('2')
 
         if (populateOptions && doc) {
             doc = await doc.populate(populateOptions)
         }
         // .populate({ path: 'reviews' })
+        console.log('3')
 
         if (!doc) {
             return next(
