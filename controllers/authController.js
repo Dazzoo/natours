0f86bcbb9001979a6f10851069f8374f97812dc6
Dazzoo <yuraks46@gmail.com>
2023-11-09 01:08:04 +0200

    fix auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,11 +40,14 @@ const signEmailToken = (email) => {
 
 const createSendToken = (statusCode, user, res, message) => {
     const token = signToken(user.id)
+    console.log('process.env.JWT_COOKIE_EXPIRES_IN', process.env.JWT_COOKIE_EXPIRES_IN)
+    console.log('process.env.JWT_COOKIE_EXPIRES_IN', process.env.JWT_COOKIE_EXPIRES_IN)
     const cookieOptions = {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        sameSite: 'none',
+        httpOnly: false,
     }
 
     if (process.env.NODE_ENVIRONMENT === 'production') {
